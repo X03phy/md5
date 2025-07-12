7edcf07aac71343712bef912c209fffc396d5f29
Elio Bonutto <elio.bonutto@gmail.com>
2025-07-12 19:22:58 +0200

    ajout de tous les fichiers et il ne manque que la fct md5_final

---
+++ src/main.c
@@ -0,0 +1,35 @@
+#include "md5.h"
+#include <openssl/md5.h>
+#include <stdio.h>
+#include <string.h>
+
+int main( int argc, char **argv )
+{
+	if ( argc != 2 )
+	{
+		printf( "Please, enter only 1 argument\n" );
+		return ( 1 );
+	}
+
+	unsigned char hash[SHA256_DIGEST_LENGTH];
+	unsigned char my_hash[SHA256_DIGEST_LENGTH];
+
+	SHA256( ( const unsigned char * ) argv[1], strlen( argv[1] ), hash );
+	printf( "openssl SHA256: " );
+	for ( uint8_t i = 0; i < SHA256_DIGEST_LENGTH; ++i )
+		printf( "%02x", hash[i] );
+	printf( "\n" );
+
+	sha256( ( const unsigned char * ) argv[1], strlen(argv[1]), my_hash );
+	printf( "my SHA256:      " );
+	for ( uint8_t i = 0; i < SHA256_DIGEST_LENGTH; ++i )
+		printf( "%02x", my_hash[i] );
+	printf("\n");
+
+	if ( !memcmp( ( const char * ) hash, ( const char * ) my_hash, SHA256_DIGEST_LENGTH ) )
+		printf( "OK\n" );
+	else
+		printf( "KO\n" );
+
+	return ( 0 );
+}
---
+++ src/md5.c
@@ -0,0 +1,114 @@
+#include "md5.h"
+
+static void md5_init( t_md5_ctx *ctx )
+{
+	ctx->state[0] = 0x67452301;
+	ctx->state[1] = 0xefcdab89;
+	ctx->state[2] = 0x98badcfe;
+	ctx->state[3] = 0x10325476;
+
+	ctx->bitlen = 0;
+	ctx->buffer_len = 0;
+}
+
+static void md5_transform( t_md5_ctx *ctx )
+{
+	uint32_t W[16];
+	uint32_t a, b, c, d;
+	uint32_t f, g, tmp;
+	uint8_t i;
+
+	a = ctx->state[0]; b = ctx->state[1]; c = ctx->state[2]; d = ctx->state[3];
+
+	for ( i = 0; i < 64; ++i )
+	{
+		if ( i < 16 )
+		{
+			f = F( b, c, d );
+			g = i;
+		}
+		else if ( i < 32 )
+		{
+			f = G( b, c, d );
+			g = ( 5 * i + 1 ) % 16;
+		}
+		else if ( i < 48 )
+		{
+			f = H( b, c, d );
+			g = ( 3 * i + 1 ) % 16;
+		}
+		else
+		{
+			f = I( b, c, d );
+			g = ( 7 * i ) % 16;
+		}
+
+		tmp = d;
+		d = c;
+		c = b;
+		b = b + LEFTROTATE( ( a + f + K[i] + W[g] ), S[i]);
+	}
+
+	ctx->state[0] += a;
+	ctx->state[1] += b;
+	ctx->state[2] += c;
+	ctx->state[3] += d;
+}
+
+static void sha256_update( t_md5_ctx *ctx, const uint8_t *data, const size_t len )
+{
+	for ( size_t i = 0; i < len; ++i )
+	{
+		ctx->buffer[ctx->buffer_len++] = data[i];
+		ctx->bitlen += 8;
+
+		if ( ctx->buffer_len == 64 )
+		{
+			md5_transform( ctx );
+			ctx->buffer_len = 0;
+		}
+	}
+}
+
+static void sha256_final( uint8_t hash[32], t_md5_ctx *ctx )
+{
+	ctx->buffer[ctx->buffer_len++] = 0x80;
+
+	if ( ctx->buffer_len > 56 )
+	{
+		while ( ctx->buffer_len < 64 )
+			ctx->buffer[ctx->buffer_len++] = 0x00;
+		ms5_transform( ctx );
+		ctx->buffer_len = 0;
+	}
+
+	while ( ctx->buffer_len < 56 )
+		ctx->buffer[ctx->buffer_len++] = 0x00;
+
+	ctx->buffer[ctx->buffer_len++] = ( ctx->bitlen >> 56 ) & 0xff;
+	ctx->buffer[ctx->buffer_len++] = ( ctx->bitlen >> 48 ) & 0xff;
+	ctx->buffer[ctx->buffer_len++] = ( ctx->bitlen >> 40 ) & 0xff;
+	ctx->buffer[ctx->buffer_len++] = ( ctx->bitlen >> 32 ) & 0xff;
+	ctx->buffer[ctx->buffer_len++] = ( ctx->bitlen >> 24 ) & 0xff;
+	ctx->buffer[ctx->buffer_len++] = ( ctx->bitlen >> 16 ) & 0xff;
+	ctx->buffer[ctx->buffer_len++] = ( ctx->bitlen >> 8 ) & 0xff;
+	ctx->buffer[ctx->buffer_len++] = ctx->bitlen & 0xff;
+	ms5_transform( ctx );
+
+	for ( uint8_t i = 0; i < 8; ++i )
+	{
+		hash[ i * 4 ] = ( ctx->state[i] >> 24 ) & 0xff;
+		hash[ i * 4 + 1 ] = ( ctx->state[i] >> 16 ) & 0xff;
+		hash[ i * 4 + 2 ] = ( ctx->state[i] >> 8 ) & 0xff;
+		hash[ i * 4 + 3 ] = ( ctx->state[i] ) & 0xff;
+	}
+}
+
+void md5( const uint8_t *data, size_t len, uint8_t hash[16] )
+{
+	t_md5_ctx ctx;
+
+	md5_init( &ctx );
+	md5_update( &ctx, data, len );
+	md5_final( hash, &ctx );
+}
